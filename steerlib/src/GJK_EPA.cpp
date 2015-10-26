9fe4b1eb623b6640cf0ee8ed710b234a0e11b2f3
Firas S <choogiesaur@users.noreply.github.com>
2015-10-25 22:56:08 -0400

    update getVector -> farthestPoint

--- steerlib/src/GJK_EPA.cpp
+++ steerlib/src/GJK_EPA.cpp
@@ -95,31 +95,33 @@ Util::Vector calculateCenter(const std::vector<Util::Vector>& _shape) {
 }
 Util::Vector support(const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB, Util::Vector direction)
 {
-	Util::Vector pointA = getVector(_shapeA, direction);
-	Util::Vector pointB = getVector(_shapeB, (direction*-1));
+	Util::Vector pointA = farthestPoint(_shapeA, direction);
+	Util::Vector pointB = farthestPoint(_shapeB, (direction*-1));
 
 	return (pointA - pointB);
 
 }
 
-Util::Vector getVector(const std::vector<Util::Vector>& _shape, Util::Vector direction)
+Util::Vector farthestPoint(const std::vector<Util::Vector>& _shape, Util::Vector direction)
 {
-	std::vector<Util::Vector>::const_iterator ret_vect = _shape.begin();
-	float dotprod = 0;
-	float greatest = -999999999;
-	
-	//calculate point farthest from direction
-	for (std::vector<Util::Vector>::const_iterator vect = _shape.begin(); vect != _shape.end(); vect++) {
-		dotprod = vect->x*direction.x + vect->y*direction.y + vect->z*direction.z;
-		if (dotprod  >greatest) {
-			greatest = dotprod;
-			ret_vect = vect;
+	//iterator to keep track of current farthest point
+	std::vector<Util::Vector>::const_iterator currentFarthest = _shape.begin();
+
+	float currentDot = 0;
+	float highestDot = -999999999;
+
+	//loop through all points in shape, find one with greatest dot product with the desired direction. will be farthest point
+	for (std::vector<Util::Vector>::const_iterator iter = _shape.begin(); iter != _shape.end(); ++iter)
+	{
+		currentDot = (iter->x * direction.x) + (iter->y * direction.y) + (iter->z * direction.z);
+		if (currentDot > highestDot)
+		{
+			highestDot = currentDot;
+			currentFarthest = iter;
 		}
-
 	}
-
-	Util::Vector returnVect(ret_vect->x, ret_vect->y, ret_vect->z);
-	return returnVect;
+	Util::Vector returnVector(currentFarthest->x, currentFarthest->y, currentFarthest->z);
+	return returnVector;
 }
 
 Util::Vector calDir(const std::vector<Util::Vector>& simplex)
