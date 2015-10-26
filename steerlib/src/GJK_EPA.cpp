df5576752636e8db3062fb65cfc8dfd0a5a6ff96
CG-F15-1-Rutgers <bkh39@scarletmail.rutgers.edu>
2015-10-25 23:27:05 -0400

    Update GJK_EPA.cpp

--- steerlib/src/GJK_EPA.cpp
+++ steerlib/src/GJK_EPA.cpp
@@ -9,23 +9,10 @@
 
 SteerLib::GJK_EPA::GJK_EPA()
 {
-	
+
 
 }
 
-
-
-//Look at the GJK_EPA.h header file for documentation and instructions
-bool SteerLib::GJK_EPA::intersect(float& return_penetration_depth, Util::Vector& return_penetration_vector, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB)
-{
-	//Simplex to be build unsing points found via Minkowski differences
-	std::vector<Util::Vector> simplex;
-
-	//flag to be set true if the simplex contains origin (0,0)
-	bool col = false;
-
-    return col; // There is no collision
-}
 
 Util::Vector calculateCenter(const std::vector<Util::Vector>& _shape) {
 	Util::Vector retVect(0, 0, 0);
@@ -83,7 +70,7 @@ Util::Vector support(const std::vector<Util::Vector>& _shapeA, const std::vector
 Util::Vector calculateDirection(const std::vector<Util::Vector>& simplex)
 {
 	std::vector<Util::Vector>::const_reverse_iterator reverse = simplex.rbegin();
-	Util::Vector last(reverse->x,reverse->y,reverse->z);
+	Util::Vector last(reverse->x, reverse->y, reverse->z);
 	reverse = reverse + 1;
 	Util::Vector nextLast(reverse->x, reverse->y, reverse->z);
 
@@ -111,16 +98,63 @@ bool GJK(const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vecto
 	if (direction == (0, 0, 0)) {
 		return true;
 	}
-	
+
 	//first 2 points on the simplex
-	simplex.push_back(support(_shapeA,_shapeB,direction));
+	simplex.push_back(support(_shapeA, _shapeB, direction));
 	direction = direction*-1;
 	simplex.push_back(support(_shapeA, _shapeB, direction));
 
 	//build the simplex
 	while (true) {
 		//get new direction
-		direction = calDir(simplex);
+		direction = calculateDirection(simplex);
+
+		//add new minkowski difference vertex
+		simplex.push_back(support(_shapeA, _shapeB, direction));
+
+		//check simplex contains origin
+
+
+	}
+
+
+
+
+	if (is_colliding) {
+		//Still searching for collision
+		return (simplex, true);
+	}
+	else {
+		//there is no collision
+		return (NULL, false);
+	}
+}
+
+//Look at the GJK_EPA.h header file for documentation and instructions
+bool SteerLib::GJK_EPA::intersect(float& return_penetration_depth, Util::Vector& return_penetration_vector, const std::vector<Util::Vector>& _shapeA, const std::vector<Util::Vector>& _shapeB)
+{
+	//Simplex to be build using points found via Minkowski differences
+	std::vector<Util::Vector> simplex;
+
+	//flag to be set true if the simplex contains origin (0,0)
+	bool is_colliding = false;
+
+	//calculates for collision
+	//gets centers of Minkowski difference viadifference of center of both shapes.
+	Util::Vector direction = calculateCenter(_shapeA) - calculateCenter(_shapeB);
+	if (direction == (0, 0, 0)) {
+		return true;
+	}
+
+	//first 2 points on the simplex
+	simplex.push_back(support(_shapeA, _shapeB, direction));
+	direction = direction*-1;
+	simplex.push_back(support(_shapeA, _shapeB, direction));
+
+	//build the simplex
+	for (int i = 0; i < 10; i++) {
+		//get new direction
+		direction = calculateDirection(simplex);
 
 		//add new minkowski difference vertex
 		simplex.push_back(support(_shapeA, _shapeB, direction));
